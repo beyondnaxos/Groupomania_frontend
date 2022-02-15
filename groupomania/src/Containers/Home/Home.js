fa6fb2fef4e41f81245682ae883053e12fb241cd
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-02-15 17:32:07 +0100

    Recup des datas + affichage des posts
    Base affichage OK
    reste à récupérer le nom d'utilisateur ainsi que les photos et leurs affichages respectifs

--- groupomania/src/Containers/Home/Home.js
+++ groupomania/src/Containers/Home/Home.js
@@ -1,47 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.css'
 import Post from '../../Components/Post/Post'
-import Showpost from '../../Components/ShowPost/ShowPost'
+// import Showpost from '../../Components/ShowPost/ShowPost'
 import { Navigate } from "react-router-dom";
 // import axios from 'axios';
 // import {  useState } from 'react'
 // import { v4 as uuidv4 } from 'uuid'
 // import { Link } from 'react-router-dom'
-
+import Idandphoto from '../../Components/IdAndPhoto/IdAndPhoto';
+import Commentpost from '../../Components/CommentPost/CommentPost';
 
 export default function Home(props) {
 
     const [posts, setPosts] = useState([])
 
-    const getPosts = (async (props) => {
-        await fetch('http://localhost:8080/api/tutorials/published')
+    useEffect(() => {
+        fetch('http://localhost:8080/api/tutorials/published')
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                setPosts(data.length)
-                console.log(posts);
+                setPosts(data)
             })
-    })()
+        return () => {
 
+        };
+    }, []);
 
-
+    console.log(posts);
 
     return (
+
         props.user ? (
 
             <>
                 <h1 className='home-title'>Bienvenue {props.username}</h1>
                 <div className='container-cards'>
 
+                    <Post setUser={props.user.username} token={props.user.token} />
+               
+                    {posts.reverse().map((data) => {
+                        return (
+                            <div className='post' key={data.id}>
+                                <Idandphoto />
+                                <p className='post-content'>{data.description}</p>
+                                
+                                <Commentpost className='allign' />
+                            </div>
+                        );
 
-                    <Post onchange={getPosts} setUser={props.user.username} token={props.user.token} />
-
-                    <Showpost setPosts={props.posts} />
-                    {/* <Showpost setPosts={props.setPosts}/> */}
-
+                    })}
 
                 </div>
-
 
             </>
         ) : (
