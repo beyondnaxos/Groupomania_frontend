af0612597d021929b8c5e49ee238569b8dc9131b
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-02-16 11:47:03 +0100

    Affichage du bon id de post + TimeAgo React OK

--- groupomania/src/Components/CommentPost/CommentPost.js
+++ groupomania/src/Components/CommentPost/CommentPost.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import './CommentPost.css';
+// import axios from 'axios';
 
-const Commentpost = () => <input type="text" placeholder='write your comment here' className='comment-post'/>
-       
+
+
+const Commentpost = (props) => {
+
+    // const [comContent, setComContent] = useState('')
+
+
+    // const postCom = async (e) => {
+    //     e.preventDefault()
+    //     const setUser = props.setUser
+    //     const config = {
+    //         headers: {
+    //             'Authorization': `Bearer ${props.token}`
+    //         }
+    //     }
+    //     console.log(setUser)
+    //     console.log(config)
+    //     let form = new FormData();
+    //     form.append('name', setUser)
+    //     form.append('text', comContent)
+    //     const response = await axios.post('http://localhost:8080/api/tutorials', form, config)
+    //     // props.setPosts([...props.posts, response.data])
+    //     console.log(response.data)
+
+    // }
+
+    return (
+        // <form className='comment-post'>
+        <input
+            type="text"
+            placeholder='write your comment here'
+            className='comment-post' />
+      
+            
+    )
+}
+
 
 export default Commentpost;
--- groupomania/src/Containers/Home/Home.js
+++ groupomania/src/Containers/Home/Home.js
@@ -7,12 +7,18 @@ import { Navigate } from "react-router-dom";
 // import {  useState } from 'react'
 // import { v4 as uuidv4 } from 'uuid'
 // import { Link } from 'react-router-dom'
-import Idandphoto from '../../Components/IdAndPhoto/IdAndPhoto';
 import Commentpost from '../../Components/CommentPost/CommentPost';
+import TimeAgo from 'javascript-time-ago'
+import ReactTimeAgo from 'react-time-ago'
+
+import en from 'javascript-time-ago/locale/en.json'
+TimeAgo.addDefaultLocale(en)
+
 
 export default function Home(props) {
 
     const [posts, setPosts] = useState([])
+
 
     useEffect(() => {
         fetch('http://localhost:8080/api/tutorials/published')
@@ -33,18 +39,26 @@ export default function Home(props) {
         props.user ? (
 
             <>
-                <h1 className='home-title'>Bienvenue {props.username}</h1>
+                <h1 className='home-title'>Bienvenue {props.user.username}</h1>
                 <div className='container-cards'>
 
                     <Post setUser={props.user.username} token={props.user.token} />
-               
+
                     {posts.reverse().map((data) => {
                         return (
                             <div className='post' key={data.id}>
-                                <Idandphoto />
+                                <div className='main-id-container'>
+                                    <div className='profil-photo-container'>
+                                        <img className='profil-photo' src='https://www.w3schools.com/howto/img_avatar.png' alt='' />
+                                    </div>
+                                    <div className='profil-id-container'>
+                                        <h3 className='profil-id'>{data.userId}</h3>
+                                        <p className='profil-time'><ReactTimeAgo className='date-time' date={data.createdAt} locale="en-US"/></p>
+                                    </div>
+                                </div>
                                 <p className='post-content'>{data.description}</p>
-                                
-                                <Commentpost className='allign' />
+                                <img src="data.imageUrl" alt="" />
+                                <Commentpost setUser={props.user.username} token={props.user.token} className='allign' />
                             </div>
                         );
 
