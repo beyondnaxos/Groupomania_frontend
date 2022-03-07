556be562ee03b1e52c3e2d829abaaea57e105b47
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-03-07 12:16:20 +0100

    gestion de l'admin regex

--- groupomania/src/Components/SignUpForm/SignUpForm.js
+++ groupomania/src/Components/SignUpForm/SignUpForm.js
@@ -6,15 +6,28 @@ import { useNavigate } from 'react-router-dom'
 
 const SignUpForm = () => {
 
+
+    const UseRegex= (input) => {
+        let regex = /admin@[a-zA-Z]+\.[a-zA-Z]+/i
+        return regex.test(input)
+    }
+
     const [emailReg, setEmailReg] = useState('')
     const [usernameReg, setUsernameReg] = useState('')
     const [passwordReg, setPasswordReg] = useState('')
     let navigate = useNavigate()
 
     const register = async (e) => {
+        if (UseRegex(emailReg)) {
         e.preventDefault();
-        const response = await Axios.post('http://localhost:8080/api/auth/signup', { email: emailReg, username: usernameReg, password: passwordReg })
+        const response = await Axios.post('http://localhost:8080/api/auth/signup', { email: emailReg, username: usernameReg, password: passwordReg, isAdmin: true})
         console.log(response);
+    }else {
+        e.preventDefault();
+        const response = await Axios.post('http://localhost:8080/api/auth/signup', { email: emailReg, username: usernameReg, password: passwordReg, isAdmin: false })
+        console.log(response);
+
+    }
         navigate('/login')
     }
 
@@ -22,6 +35,7 @@ const SignUpForm = () => {
         navigate('/login')
     }
 
+    
 
 
     return (
