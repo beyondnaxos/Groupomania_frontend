e3f59c674469efb086d3de216dab112cbbc1d3dc
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-03-10 12:16:41 +0100

    Supression des commentaires ok sur bakc / state non actualisé sur front

--- groupomania/src/Components/showComments/showComments.js
+++ groupomania/src/Components/showComments/showComments.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import axios from 'axios';
 import './showComments.css'
 
 const Showcomments = (props) => {
     const comData = props.comData
-
+    const handleDeleteComment = async (e) => {
+    
+        e.preventDefault()
+        const id = props.postId
+        console.log(comData);
+        const commentId = props.comData.id
+        
+        // const userId = props.userId
+        // console.log('last clg' + setId, userId)
+        const config = {
+          headers: {
+            'Authorization': `Bearer ${props.token}`
+          }
+        }
+    
+        const response = await axios.delete(`http://localhost:8080/api/tutorials/${id}/comment/${commentId}`, config)
+        console.log(response)
+        props.setPost((oldState) => {
+            const posts = [...oldState]
+            
+            
+          return posts
+        })
+    
+    }
             return (
                 <div className='comment' key={comData.key}>
                     <div className='comment-id-container'>
                         <h3 className='comment-id'>{comData.name}</h3>
                         <p className='comment-content'>{comData.text}</p>
+                    </div>
+                    <div className='comment-button-container'>
+                        <button className='comment-button' onClick={handleDeleteComment}>Delete</button>
+
                     </div>
                 </div>
             )
